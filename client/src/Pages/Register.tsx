412b9ab96ae912217cd6b3fdc8eec20475a11779
yebin Park <yebinp1102@naver.com>
2022-11-27 10:46:29 +0900

    add style on register form

--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -58,7 +58,7 @@ const Register: React.FC = () => {
   return (
     <div className='registerWrap' style={{backgroundImage: `url(${bgimage})`}}>
       <div className='registerContainer'>
-        <form>
+        <form className='registerForm'>
           {/* 유저명 */}
           <label htmlFor='username'>
             유저명 :
@@ -96,7 +96,7 @@ const Register: React.FC = () => {
             onBlur={() => setPwdFocus(false)}
           />
           <p id='pwdnote' className={pwdFocus && !validPwd ? 'instructions' : 'offscreen'}>
-            <FcInfo /> 비밀번호는 반드시 하나 이상의 대소문자, 특수문자와 숫자를 포함해야 하며, 총 8 ~ 24자여야 합니다.
+            <FcInfo /> 비밀번호는 반드시 하나 이상의 대문자, 소문자, 특수문자, 그리고 숫자를 포함해야 하며, 총 8 ~ 24자여야 합니다.
           </p>
 
           {/* 비밀번호 확인 */}
@@ -122,11 +122,9 @@ const Register: React.FC = () => {
           <button disabled={!validName || !validPwd || !validMatch ? true : false}>회원가입</button>
         </form>
 
-        <p>
-          이미 회원이신가요 ? <br/>
-          <span className='line'>
-            <Link to='/login'>로그인 페이지로 이동하기</Link>
-          </span>
+        <p className='toLogin'>
+          <div>이미 회원이신가요 ?</div>
+          <Link to='/login'>로그인 페이지로 이동하기</Link>
         </p>
       </div>
     </div>

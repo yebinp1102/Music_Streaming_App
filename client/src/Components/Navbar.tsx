adeece9350b3c213af0b210a083b18bd25138a7d
yebin Park <yebinp1102@naver.com>
2023-01-15 12:44:22 +0900

    FIX: if token is expired browser will logout itself

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './CSS/Navbar.css'
 import { Link } from 'react-router-dom'
+import decode from 'jwt-decode'
 
 const Navbar:React.FC = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile') as any))
@@ -10,6 +11,17 @@ const Navbar:React.FC = () => {
     setUser(null);
     window.location.reload();
   }
+
+  // Navbar 컴포넌트는 렌더링 될 때마다 토큰이 존재하는지 확인
+  useEffect(() => {
+    const token = user?.data?.token
+    if(token){
+      // decode 함수는 이 토큰이 언제 만료되는지에 대한 정보를 가짐
+      const decodedToken : any = decode(token)
+      // 토큰이 만료된 경우 자동 로그아웃
+      if(decodedToken.exp * 1000 < new Date().getTime()) logout()
+    }
+  },[])
   return (
     <div className='container'>
       <div className='wrap'>
